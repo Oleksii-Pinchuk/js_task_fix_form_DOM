06f5c905fb5ea0d5a06a33550be6235d3137593d
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-04-07 14:15:57 +0300

    form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('.field-text');
+
+[...allInputs].map(input => {
+  const upperedName = input.name[0].toUpperCase()
+    + input.name.slice(1).replace(/(\B[A-Z])/g, ' $1');
+
+  input.insertAdjacentHTML('beforebegin', `
+    <label class="field-label" for=${input.id}>
+      ${upperedName}
+    </label>`
+  );
+
+  input.placeholder = upperedName;
+});
